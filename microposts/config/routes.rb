f25d96a9bb53710e0ef8982d541d108414d2bd5e
minami.terada <s2jvrnp10vc@i.softbank.jp>
2019-02-13 16:12:09 +0000

    user login

--- microposts/config/routes.rb
+++ microposts/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
@@ -14,6 +13,7 @@ Rails.application.routes.draw do
     end
     collection do
       get :search
+    end
   end
   
   resources :microposts, only: [:create, :destroy]
